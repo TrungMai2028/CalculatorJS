5bd51e900238f7ccc93445a8dd4afebbede99574
TrungMai <bieuluu53@gmail.com>
2024-12-16 09:58:46 -0500

    create operation func

--- javascript.js
+++ javascript.js
@@ -3,7 +3,21 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
-
+//create operation func
+function operation (num1, num2, operator){
+    if (operator === "+"){
+        add(num1, num2);
+    }
+    if (operator === "-"){
+        subtract(num1, num2);
+    }
+    if (operator === "*"){
+        multiply(num1, num2);
+    }
+    if (operator === "/"){
+        divide(num1, num2);
+    }
+}
 
 
 //basic math operator
