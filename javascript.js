ea50977a71428b6eb9aa5baade8c4be72ccffb17
TrungMai <bieuluu53@gmail.com>
2024-12-17 16:32:57 -0500

    display on cal screen

--- javascript.js
+++ javascript.js
@@ -1,22 +1,24 @@
 //create three variable
-let num1 = 0;
-let num2 = 0;
-let operator = "";
+let num1 = null;
+let num2 = null;
+let operator = null;
+let displayValue = "";
 
 //create operation func
 function operation (num1, num2, operator){
     if (operator === "+"){
-        add(num1, num2);
+        return add(num1, num2);
     }
     if (operator === "-"){
-        subtract(num1, num2);
+        return subtract(num1, num2);
     }
     if (operator === "*"){
-        multiply(num1, num2);
+        return multiply(num1, num2);
     }
     if (operator === "/"){
-        divide(num1, num2);
+        return divide(num1, num2);
     }
+    return null;
 }
 
 
@@ -28,3 +30,17 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+//function to update display
+function updateDisplay(){
+    const display = document.querySelector(".display");
+    display.textContent = displayValue;
+}
+
+// Function to handle button clicks
+function handleButtonClick(event) {
+    //if 
+}
+
+// Add event listeners to buttons
+
